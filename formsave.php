16c31eaab0e8171002df89d13122f8ed4cb2d6af
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-04 09:36:48 +1100

    Initial commit

---
+++ formsave.php
@@ -0,0 +1,7 @@
+<?php
+
+    // $aa = $_POST['name'];
+    // $bb = $_POST['email'];
+
+    var_dump($_POST);
+?>
---
+++ notic.php
@@ -0,0 +1,5 @@
+<?php
+    var_dump($_POST);
+    $content = $_POST['content'];
+    $user = $_POST['user'];
+?>
