b7245ae4b484d5c25624f840a18f919fa80e91ba
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-04 10:41:34 +1100

    Update notic.php
    more condition

--- notic.php
+++ notic.php
@@ -2,4 +2,30 @@
     var_dump($_POST);
     $content = $_POST['content'];
     $user = $_POST['user'];
+
+    $n = [
+        'qwe',
+        'asd',
+        'zxc'
+    ];
+
+    if($content == ''){
+        die("留言不能为空");
+    }
+
+    if( $user == '' ){
+        die("user不能为空");
+    }
+
+    foreach( $n as $name){
+        if( $user == $name ){
+            die("不能使用该用户名");
+        }
+    }
+
+    // for ($i=0; $i<10; $i++){
+    //     echo $i;
+    //     echo "<br>";
+    // }
+
 ?>
